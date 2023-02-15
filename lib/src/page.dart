6a985c64906f02a0b2119c24f4154969d5b89ccd
suvaysov <suvaysov@tehnosk.ru>
2023-02-15 22:14:11 +0600

    offset default

--- lib/src/page.dart
+++ lib/src/page.dart
@@ -73,6 +73,7 @@ class _PDFPageState extends State<PDFPage> {
       panLimit: widget.panLimit,
       maxScale: widget.maxScale,
       initialScale: widget.initialScale,
+      initialOffset: Offset(0,100),
       child: Image(image: provider),
     );
   }
